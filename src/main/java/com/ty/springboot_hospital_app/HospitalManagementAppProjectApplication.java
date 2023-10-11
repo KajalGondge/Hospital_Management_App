49ed958f2123db80f2068995cb3b0fbd2c0c607d
Kajal <Kajal@DESKTOP-DEGTKLJ>
2023-10-11 10:38:37 +0530

    Initial Commit

---
+++ src/main/java/com/ty/springboot_hospital_app/HospitalManagementAppProjectApplication.java
@@ -0,0 +1,13 @@
+package com.ty.springboot_hospital_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HospitalManagementAppProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HospitalManagementAppProjectApplication.class, args);
+	}
+
+}
