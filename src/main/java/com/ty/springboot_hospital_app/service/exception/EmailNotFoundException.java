49c367014d82c4596686643ec79aa4f89db4fd6b
Kajal <Kajal@DESKTOP-DEGTKLJ>
2024-01-25 11:57:43 +0530

    New Commit

---
+++ src/main/java/com/ty/springboot_hospital_app/service/exception/EmailNotFoundException.java
@@ -0,0 +1,20 @@
+package com.ty.springboot_hospital_app.service.exception;
+
+public class EmailNotFoundException extends RuntimeException
+{
+	
+			private String message ="Email Not Found";
+
+			public EmailNotFoundException(String message) {
+				super();
+				this.message = message;
+			}
+			
+			@Override
+			public String getMessage() {
+			// TODO Auto-generated method stub
+			return message;
+			}
+
+	
+}
