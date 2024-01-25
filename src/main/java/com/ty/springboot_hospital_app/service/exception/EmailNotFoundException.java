0c9f1912236c0ba0f3564f99e83a54a70104796f
Kajal <Kajal@DESKTOP-DEGTKLJ>
2024-01-25 13:56:44 +0530

    Class Added

--- src/main/java/com/ty/springboot_hospital_app/service/exception/EmailNotFoundException.java
+++ src/main/java/com/ty/springboot_hospital_app/service/exception/EmailNotFoundException.java
@@ -2,7 +2,7 @@ package com.ty.springboot_hospital_app.service.exception;
 
 public class EmailNotFoundException extends RuntimeException
 {
-	
+
 	private String message ="Email Not Found";
 
 	public EmailNotFoundException(String message) {
@@ -15,6 +15,4 @@ public class EmailNotFoundException extends RuntimeException
 	// TODO Auto-generated method stub
 	return message;
 	}
-
-	
 }
